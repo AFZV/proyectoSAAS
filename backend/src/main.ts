d30591fe2188659aa05e77889efffcf7b88ec950
AlexisZuluaga93 <zuluagaalexis93@gmail.com>
2026-02-22 19:43:54 -0500

    cambios en puerto dinamoco

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -39,7 +39,7 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-  await app.listen(4001, '0.0.0.0');
+  await app.listen(process.env.PORT || 4000, '0.0.0.0');
 }
 
 bootstrap();
